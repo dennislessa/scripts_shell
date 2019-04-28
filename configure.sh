6b691de2fc83fdde1d150c419915702e3bb65365
Dennis Lessa <dennislessa.dourado@gmail.com>
2019-04-28 14:15:31 -0300

    v1.1.1
    Script para instalação Debian 9.9 KDE (uso de funções).

--- configure.sh
+++ configure.sh
@@ -1,38 +1,79 @@
 #!/bin/bash
 
-if [ $(id -u) -eq 0 ]; then
-    echo "Removendo configurações padrão..."
-    apt purge sddm-theme* plasma-workspace-wallpapers
-    rm -rf /usr/share/wallpapers/*
-    
-    dpkg --add-architecture i386
-    
+remove_packages() {
+	echo "Removing unecessary packages..."
+	apt autoremove -y
+}
+
+update_system() {
     echo "Atualizando o sistema..."
     apt update && apt dist-upgrade -y
     
     echo "Atualizando o Grub..."
     update-grub && update-grub2
-    
-    echo "Instalando programas..."
+}
+
+thin_kde() {
+    echo "Installing thin KDE..."
+    apt purge *kde* *plasma* *firefox* *libreoffice* *openjdk* *icedtea* -y
+
+    apt install plasma-desktop \
+                plasma-nm \
+                plasma-widgets-addons \
+                kde-l10n-ptbr \
+                firefox-esr \
+                firefox-esr-l10n-pt-br \
+                libreoffice \
+                libreoffice-l10n-pt-br \
+                libreoffice-style-breeze \
+                openjdk-8-jdk \
+                openjdk-8-jre \
+                ark \
+                gwenview \
+                okular \
+                kio-mtp \
+                vlc \
+                phonon-backend-vlc -y
+}
+
+general_packages() {
+    echo "Installing general packages..."
+
     apt install linux-headers-$(uname -r) \
                 linux-image-$(uname -r) \
                 firmware-linux \
                 firmware-samsung \
-                firmware-iwlwifi \
-                firmware-realtek \
                 firmware-intel-sound \
                 intel-microcode \
                 inteltool \
                 intel-gpu-tools \
                 build-essential \
-                phonon-backend-vlc \
-                sddm-theme-maui \
-                plasma-workspace-wallpapers -y
-    
-    echo "Atualizando o novos pacotes..."
-    apt update && apt dist-upgrade -y
+                chromium \
+                chromium-l10n \
+                ktorrent \
+                gimp \
+                inkscape \
+                partitionmanager -y
+}
+
+if [ $(id -u) -eq 0 ]; then
+    dpkg --add-architecture i386
+
+    # Update system
+    update_system
+
+    # Install general pakages
+    general_packages
+
+    # Install thin kde
+    thin_kde
+
+    # Clean the system
+    remove_packages
+
+    # Update system
+    update_system
 else
     echo "Você precisa ter permissão de administrador."
     echo "Instalação cancelada."
-    exit;
-fi
+fi;
